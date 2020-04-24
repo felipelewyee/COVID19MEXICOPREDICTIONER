45dca04982d4f60c045140afa6d87777045a0130
Juan Felipe Huan Lew Yee <felipe.lew.yee@gmail.com>
2020-04-24 12:09:29 -0500

    Agregamos actualizador para agregar datos en tiempo real de países con script https://github.com/gohkokhan/covid19_JHU_dashboard

---
+++ daily_updater.py
@@ -0,0 +1,30 @@
+#Countries to update in the databse (selected by the user)
+countries = 'Brazil','Georgia','Greece','North Macedonia','Norway','Pakistan','Romania','Mexico'
+
+#Open the database
+f1 = open('database_confirmed.csv')
+
+#Loop over countries
+for line1 in f1:
+    country = line1.split(',')[1]
+
+    #Check if the country is in the list to updates
+    if(country in countries):
+    
+        #Open the database
+        f2 = open('JHU_new3.json')
+
+        #Look for the country and take the number of confirmed cases
+        for line2 in f2:
+            if(country in line2):
+                confirmed = line2.replace('\"attributes\":','').replace('{','').replace('}','').replace('\'','').split(',')[6].split(':')[1]
+                break
+        f2.close()
+
+        #Check if the number is not already listed in the database and add it
+        if confirmed not in line1:
+            print(line1.replace('\n','')+','+confirmed)
+        else:
+            print(line1.replace('\n',''))
+    else:    
+        print(line1.replace('\n',''))
--- processing.sh
+++ processing.sh
@@ -5,3 +5,9 @@ sed -i -e 's/Korea, South/Korea South/g' time_series_covid19_confirmed_global.cs
 sed -i -e 's/Bonaire, Sint Eustatius and Saba/Bonaire Sint Eustatius and Saba/g' time_series_covid19_confirmed_global.csv
 sed -i -e 's/Taiwan\*/Taiwan/g' time_series_covid19_confirmed_global.csv
 python preprocessing.py > database_confirmed.csv
+
+#Script de covid19_JHU_dashboard para actualizar manualmente la base de datos con la informacion mas reciente. Tomado de https://github.com/gohkokhan/covid19_JHU_dashboard
+./get-data-from-JHU-dashboard.sh
+python daily_updater.py > database_confirmed_new.csv
+mv database_confirmed_new.csv database_confirmed.csv
+rm *.json
